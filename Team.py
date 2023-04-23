9293987ff89ba3f260311963473c8bad6a335cdd
dean-south <solomon.mdean@gmail.com>
2023-04-23 21:34:47 +0200

    added a comment to Team.py
    When ever you use the update_team_position always input a 2d array or list

--- Team.py
+++ Team.py
@@ -13,10 +13,18 @@ class Team:
     def add_player(self, player):
         self.team = np.append(self.team, player)
 
+    # when you add a new_team_positions it must be a 2d array
     def update_team_position(self, new_team_positions):
-
         for player in range(len(self.team)):
             self.team[player].change_position(new_team_positions[player][0], new_team_positions[player][1])
 
     def update_score(self):
         self.score += 1
+
+
+team = Team()
+player = Player('', 0, 0)
+
+team.add_player(player)
+
+team.update_team_position([[1, 1]])
