a1e8a87f9c4621c7c6fb146e5fd2b62b7591be06
dean-south <solomon.mdean@gmail.com>
2023-04-23 17:28:13 +0200

    cleaned up branch
    Only the Team and Field_Objects Classes are left

--- Field_Objects.py
+++ Field_Objects.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pygame
 import numpy as np
 
@@ -20,18 +21,75 @@ class FieldObjects(pygame.sprite.Sprite):
 
     def print_centre(self):
         print(self.rect.center)
+=======
+import math
+import numpy as np
+
+
+class FieldObjects():
+
+    centre = np.array([])
+    sprite_file_path = ""
+
+    def __init__(self, image_path, x, y):
+        self.sprite_file_path = image_path
+        self.centre = np.append(self.centre, [x,y])
+
+    def change_position(self, x, y):
+        self.centre = np.array([x, y])
+
+    def change_sprite_file_path(self, new_file_path):
+
+        self.sprite_file_path = new_file_path
+
+    def x(self):
+        return self.centre[0]
+
+    def y(self):
+        return self.centre[1]
+
+>>>>>>> Stashed changes
 
 
 class Player(FieldObjects):
 
+<<<<<<< Updated upstream
     def __init__(self, team1_sprite_path, pos_x, pos_y):
         super().__init__(team1_sprite_path, pos_x, pos_y)
         self.distance_to_ball = np.zeros([2])
 
     def print_centre(self):
         print(self.rect.center)
+=======
+    distance_to_ball = 0
+
+    def set_distance_to_ball(self, ball):
+        return math.sqrt((self.x() - ball.x()) ** 2 + (self.y() - ball.y()) ** 2)
+>>>>>>> Stashed changes
 
 
 class Ball(FieldObjects):
 
+<<<<<<< Updated upstream
     closest_player = ""
+=======
+    closest_player = -1
+
+    def find_closest_player(self, all_players):
+
+        distance = 1000000000
+        i = 0
+        player_i = 0
+        for player in all_players:
+
+            if distance > math.sqrt((self.x() - player.x()) ** 2 + (self.y() - player.y()) ** 2):
+
+                player_i = i
+
+                distance = math.sqrt((self.x() - player.x()) ** 2 + (self.y() - player.y()) ** 2)
+
+            i += 1
+
+        self.closest_player = player_i
+
+>>>>>>> Stashed changes
--- Team.py
+++ Team.py
@@ -1,12 +1,33 @@
+<<<<<<< Updated upstream
 import pygame
 import numpy as np
+=======
+import numpy as np
+from Field_Objects import *
+>>>>>>> Stashed changes
 
 
 class Team:
 
+<<<<<<< Updated upstream
     def __init__(self):
         self.team = np.array([])
         self.score = 0
 
     def add_player(self, player):
         self.team = np.append(self.team, player)
+=======
+    team = np.array([])
+
+    def __init__(self):
+        self.score = 0
+
+    def add_player(self, player):
+        self.team = np.append(self.team, player)
+
+    def change_team_position(self, new_team_positions):
+
+        for player in range(len(self.team)):
+            self.team[player].change_position(new_team_positions[player][0], new_team_positions[player][1])
+
+>>>>>>> Stashed changes
