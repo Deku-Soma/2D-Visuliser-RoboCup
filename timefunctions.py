a79780a2547f76d365a984b58e8c45cea5fdea65
Manisha0144 <127530911+Manisha0144@users.noreply.github.com>
2023-04-25 18:52:46 +0200

    Updated timer function and add the time functions

--- timefunctions.py
+++ timefunctions.py
@@ -1,74 +1,86 @@
 import time
-import sys
-sys.setrecursionlimit(6000)
-class SoccerTimer:
-    def __init__(self):
-        self.elapsed = 0
-        self.paused = False
-        self.speedup = False
-        self.start_time = None
-        self.pause_start = None
-        self.pause_time = 0
+import tkinter as tk
 
-    def start_timer(self):
-        self.start_time = time.monotonic()
-        self.run_timer()
+class Timer:
+    def __init__(self, master):
+        # Initialize variables
+        self._start = 0
+        self._elapsed = 0
+        self._paused_time = 0
+        self._paused = False
+        self._speed = 1.0
 
-    def run_timer(self):
-        if self.paused:
-            time.sleep(0.1)
-            self.run_timer()
-            return
+        # Create button and pack it
+        self._pause_button = tk.Button(master, text="Pause", command=self.pause)
+        self._pause_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-        self.elapsed = time.monotonic() - self.start_time - self.pause_time
-        if self.speedup:
-            self.elapsed *= 2
-        if self.elapsed >= 5400:  # stop the timer if it exceeds 90 minutes
-            return
-        mins = int(self.elapsed // 60)
-        secs = int(self.elapsed % 60)
-        print(f"Time elapsed: {mins:02}:{secs:02}")
-        time.sleep(0.1)
-        self.run_timer()
+        # Create second button and pack it
+        self._resume_button = tk.Button(master, text="Resume", command=self.resume)
+        self._resume_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-    def pause_timer(self):
-        self.paused = True
-        self.pause_start = time.monotonic()
+        # Create third button and pack it
+        self._rewind_button = tk.Button(master, text="Rewind", command=self.rewind)
+        self._rewind_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-    def unpause_timer(self):
-        self.paused = False
-        self.pause_time += time.monotonic() - self.pause_start
+        # Create fourth button and pack it
+        self._speedup_button = tk.Button(master, text="Speed Up", command=self.speedup)
+        self._speedup_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-    def toggle_speedup(self):
-        self.speedup = not self.speedup
+        # Create label to display timer
+        self._timer_label = tk.Label(master, text="0:00")
+        self._timer_label.pack(side=tk.TOP, fill=tk.X)
 
-    def rewind_timer(self):
-        if self.elapsed > 10:
-            self.elapsed -= 10
-        else:
-            self.elapsed = 0
+        # Keep updating the timer display
+        self._update()
 
-    def fast_forward_timer(self):
-        self.elapsed += 10
+    def start(self):
+        # Start the timer
+        self._start = time.monotonic()
 
+    def stop(self):
+        # Stop the timer and reset variables
+        self._start = 0
+        self._elapsed = 0
+        self._paused_time = 0
+        self._paused = False
+        self._speed = 1.0
 
-# Create a new soccer timer
-timer = SoccerTimer()
+    def pause(self):
+        # Pause the timer
+        self._paused = not self._paused
 
-# Start the timer
-timer.start_timer()
+    def resume(self):
+        # Resume the timer
+        if self._paused:
+            self._start += time.monotonic() - self._paused_time
+            self._paused_time = 0
+            self._paused = False
 
-# Pause the timer
-timer.pause_timer()
+    def rewind(self):
+        # Rewind the timer by 10 seconds
+        self._start -= 30
 
-# Unpause the timer
-timer.unpause_timer()
+    def speedup(self):
+        # Double the speed of the timer
+        self._speed *= 2
 
-# Toggle speedup mode
-timer.toggle_speedup()
+    def slowdown(self):
+        # Halve the speed of the timer
+        self._speed /= 2
 
-# Rewind the timer by 10 seconds
-timer.rewind_timer()
+    def _update(self):
+        # Update the timer display
+        if self._start is not None:
+            elapsed_time = time.monotonic() - self._start
+            if not self._paused:
+                elapsed_time /= self._speed
+            else:
+                self._paused_time += 1
+            elapsed_time = min(elapsed_time, 20*60) # limit elapsed time to 20 minutes
+            minutes, seconds = divmod(elapsed_time, 60)
+            self._timer_label.config(text=f"{int(minutes)}:{int(seconds):02d}")
+        self._timer_label.after(1000, self._update) # update every second
 
-# Fast forward the timer by 10 seconds
-timer.fast_forward_timer()
+# Create the main window
+root = tk.Tk()
+root.title("Soccer Game Timer")
