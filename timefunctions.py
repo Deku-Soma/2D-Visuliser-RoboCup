e843ed1e9cc784c297a9cc949196ff9bbad81b83
dean-south <solomon.mdean@gmail.com>
2023-04-29 20:12:25 +0200

    1.0 Version

--- timefunctions.py
+++ timefunctions.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 
-# Set the duration of the game in seconds
-GAME_DURATION = 90 * 60 #or (20 * 60) for the match to end in 20 minutes
 
 class Timer:
-    def __init__(self, master):
+    # Set the duration of the game in seconds
+
+    GAME_DURATION = 90 * 60  # or (20 * 60) for the match to end in 20 minutes
+
+    def __init__(self, master, print_timer):
         self.master = master
-        self.remaining = GAME_DURATION
-        self.timer_label = tk.Label(master, text=self.format_time(GAME_DURATION))
+        self.remaining = self.GAME_DURATION
+        self.timer_label = tk.Label(master, text=self.format_time(self.GAME_DURATION))
         self.timer_label.pack()
         self.start_button = tk.Button(master, text="Start", command=self.start_timer)
         self.start_button.pack(side=tk.LEFT, padx=5)
@@ -17,31 +19,40 @@ class Timer:
         self.rewind_button.pack(side=tk.LEFT, padx=5)
         self.speedup_button = tk.Button(master, text="Speed Up", command=self.speedup_timer)
         self.speedup_button.pack(side=tk.LEFT, padx=5)
-    
+
+        self.speed_up = 1
+
     def start_timer(self):
         self.timer_tick()
-    
-    def stop_timer(self): 
+
+    def stop_timer(self):
         # Pause function
         self.master.after_cancel(self.timer)
-    
+
+        self.speed_up = 1
+
     def rewind_timer(self):
-        self.remaining += 60 # rewind by one minute
+        self.remaining += 60  # rewind by one minute
         self.timer_label.configure(text=self.format_time(self.remaining))
-    
+
     def speedup_timer(self):
-        self.remaining -= 60 # speed up by one minute
-        self.timer_label.configure(text=self.format_time(self.remaining))
-    
+
+        self.speed_up += 1
+
+        '''self.remaining -= 60 # speed up by one minute
+        self.timer_label.configure(text=self.format_time(self.remaining))'''
+
     def timer_tick(self):
-        self.remaining -= 1
+        self.remaining -= 1 * self.speed_up
+
+        print_time.print(self.remaining)
         self.timer_label.configure(text=self.format_time(self.remaining))
         if self.remaining <= 0:
             self.timer_label.configure(text="Game over!")
         else:
             # ensures that a 90 minute game is completed in 20 minutes
-            self.timer = self.master.after(250, self.timer_tick) # increment timer by a factor of 4.5 (90/20)
-    
+            self.timer = self.master.after(1000, self.timer_tick)  # increment timer by a factor of 4.5 (90/20)
+
     def format_time(self, seconds):
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
@@ -51,9 +62,27 @@ class Timer:
 root = tk.Tk()
 root.title("Soccer Game Timer")
 # Create the Timer object inside the window
-timer = Timer(root)
-#timer.start()
 
+# timer.start()
+
+class PrintTime:
+
+    def __init__(self, master):
+        self.master = master
+        self.time = 0
+
+        self.print_time_label = tk.Label(master)
+        self.print_time_label.pack()
+
+    def print(self, time):
+        self.time = time
+        print(self.time)
 # Start the main event loop
+
+
+print_time = PrintTime(root)
+
+timer = Timer(root, print_time)
+
 root.mainloop()
 
