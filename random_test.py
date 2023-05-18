87f83a47e5469038233281c5e14108ef9203520d
Deku-Soma <sashinsamlal@gmail.com>
2023-05-18 16:11:57 +0200

    added a skipif clause to prevent circleCI from breaking

--- random_test.py
+++ random_test.py
@@ -1,11 +1,15 @@
 import pytest
 import os
-
-def test_gui_operation():
-    if os.environ.get('DISPLAY') is None:
-        pytest.skip("Skipping GUI test in headless environment")
-        
 from Display_menu_visualiser import add
-def test_addition():
-    result = add(2, 3)
-    assert result == 5
+
+@pytest.mark.skipif(os.environ.get('DISPLAY') is None, reason="Skipping GUI test in headless environment")
+def test_gui_operation():
+    # Rest of the test code involving GUI operations
+    # ...
+    # ...
+    def test_addition():
+        result = add(2, 3)
+        assert result == 5
+
+
+
