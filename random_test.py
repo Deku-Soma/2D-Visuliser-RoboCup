a0b92f2a903d22c33b1879a08f97a7ed34fb8bf4
Deku-Soma <sashinsamlal@gmail.com>
2023-05-18 16:04:45 +0200

    change to random test, so that it skips gui related tests

--- random_test.py
+++ random_test.py
@@ -1,4 +1,10 @@
 import pytest
+import os
+
+def test_gui_operation():
+    if os.environ.get('DISPLAY') is None:
+        pytest.skip("Skipping GUI test in headless environment")
+        
 from Display_menu_visualiser import add
 def test_addition():
     result = add(2, 3)
